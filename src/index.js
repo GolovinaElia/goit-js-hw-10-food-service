7078011fd0a1f97b575184f621238f50ee85b9a4
GolovinaElia <eliagolovina@ukr.net>
2021-02-23 15:13:53 +0200

    Добавляет файлы

--- src/index.js
+++ src/index.js
@@ -7,18 +7,43 @@ const Theme = {
 };
 
 const themeRef = document.querySelector('#theme-switch-toggle');
-themeRef.addEventListener('change', handleThemeInput);
+themeRef.addEventListener('change', changeTheme);
+// document.body.classList.add(Theme['LIGHT']);
 
-document.body.classList.add(Theme['LIGHT']);
+if (localStorage.getItem('Theme')) {
+  document.body.classList.add((Theme['DARK']));
+}
+function changeTheme(event) {
+  // document.body.classList.toggle(Theme['LIGHT']); 
+  // document.body.classList.toggle(Theme['DARK']);
 
-function handleThemeInput(event) {
-    document.body.classList.toggle(Theme['LIGHT']); 
-    document.body.classList.toggle(Theme['DARK']);
-  
-    localStorage.setItem('Theme', Theme['LIGHT']); 
-    if (event.target.checked) {
-        event.preventDefault();
-     localStorage.setItem('Theme', Theme['DARK']);    
-    } 
-};
- 
+  //  if (event.target.checked) {
+  //    localStorage.setItem('Theme', (Theme['DARK'])); 
+  //    document.body.classList.add(Theme['DARK']);
+  //    localStorage.removeItem('Theme', (Theme['LIGHT']));
+  //    document.body.classList.remove(Theme['LIGHT']);
+  // } else {
+  //    localStorage.setItem('Theme', (Theme['LIGHT'])); 
+  //    document.body.classList.add(Theme['LIGHT']);
+  //    localStorage.removeItem('Theme', (Theme['DARK'])); 
+  //    document.body.classList.remove(Theme['DARK']);
+  // }
+
+  if (document.body.classList.contains(Theme['DARK'])) {
+    document.body.classList.add(Theme['LIGHT']);
+    localStorage.removeItem('Theme', (Theme['DARK']));
+    document.body.classList.remove(Theme['DARK']);
+     
+  } else {
+     event.target.checked = 'true';
+    localStorage.setItem('Theme', (Theme['DARK']));
+    document.body.classList.add(Theme['DARK']);
+    document.body.classList.remove(Theme['LIGHT']);
+  };
+  console.log(event.target.checked);
+  // if (document.body.classList.contains(Theme['DARK'])) {
+  //   document.body.classList.add(Theme['DARK']);
+  // } else {
+  //   document.body.classList.add(Theme['LIGHT']);
+  // };
+}; 
