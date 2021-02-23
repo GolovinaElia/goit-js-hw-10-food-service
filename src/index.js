8fb3df61f3a702f442a1795e1868a4a79911c85d
GolovinaElia <eliagolovina@ukr.net>
2021-02-23 21:52:23 +0200

    Добавляет файлы

--- src/index.js
+++ src/index.js
@@ -7,26 +7,24 @@ const Theme = {
 };
 
 const themeRef = document.querySelector('#theme-switch-toggle');
-themeRef.addEventListener('change', changeTheme);
+const currentTheme = localStorage.getItem('Theme');
 
-if (localStorage.getItem('Theme', (Theme['DARK']))) {
-  document.body.classList.add((Theme['DARK']));
-} else {
-  localStorage.setItem('Theme', (Theme['LIGHT']));
+if (currentTheme) {
+    document.body.classList.add(currentTheme);
+};
+if (currentTheme === Theme.dark) {
+    checkBox.checked = true;
 };
 
-function changeTheme(event) {
-  if (document.body.classList.contains(Theme['DARK'])) {
-    document.body.classList.add(Theme['LIGHT']);
-    localStorage.removeItem('Theme', (Theme['DARK']));
-    document.body.classList.remove(Theme['DARK']);
-    // localStorage.setItem('Theme', (Theme['LIGHT'])); 
-  } else {
-     event.target.checked = true;
-    localStorage.setItem('Theme',(Theme['DARK']));
-    document.body.classList.add(Theme['DARK']);
-    document.body.classList.remove(Theme['LIGHT']);
-    // localStorage.removeItem('Theme',(Theme['LIGHT']));
+themeRef.addEventListener('change', (event) => {
+    if (event.target.checked) {
+        document.body.classList.remove(Theme['LIGHT']);
+        localStorage.setItem('Theme',(Theme['DARK']));
+      document.body.classList.add(localStorage.getItem('Theme'));
+      
+    } else {
+        document.body.classList.remove(Theme['DARK']);
+        localStorage.setItem('Theme', (Theme['LIGHT']));
+        document.body.classList.add(localStorage.getItem('Theme'));
   };
-  };
- 
+});
