9cf323d1571b18bf0bf420772821f1efe126d459
GolovinaElia <eliagolovina@ukr.net>
2021-02-23 22:43:34 +0200

    Updates

--- src/js/templating.js
+++
@@ -1,7 +0,0 @@
-import items from '../templates/food-items.hbs';
-import food from '../menu.json';
-
- export const markup = items(food);
-
-export const menuRef = document.querySelector('.js-menu');
-menuRef.insertAdjacentHTML('beforeend', markup);
